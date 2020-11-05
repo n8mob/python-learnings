6a38364f051e28cec73ba8f17b32ef867b78fee3
Nate Grigg <nate.grigg@endurance.com>
2020-11-05 15:46:02 -0700

    joining lists

---
+++ test_list_append.py
@@ -0,0 +1,58 @@
+from unittest import TestCase
+
+
+class TestListAppend(TestCase):
+    def test_flat_map(self):
+        a = [1, 2]
+        b = [3, 4]
+        c = [1, 2, 3, 4]
+
+        self.assertEqual(a + b, c)
+
+    def test_append(self):
+        a = [1, 2]
+        b = 3
+        c = [1, 2, 3]
+
+        a.append(b)
+
+        self.assertEqual(a, c)
+
+    def test_list_add(self):
+        a = [1, 2]
+        b = [3, 4]
+        c = [1, 2, 3, 4]
+
+        self.assertEqual(a + b, c)
+
+    def test_add_empty(self):
+        a = [1, 2]
+        b = []
+
+        self.assertEqual(a, a + b)
+
+    def test_prepend_list(self):
+        a = 1
+        b = [2, 3]
+        c = [1, 2, 3]
+
+        b.insert(0, a)
+
+        self.assertEqual(b, c)
+
+    def test_unpacking(self):
+        a = 1
+        b = [2, 3]
+        c = [1, 2, 3]
+
+        d = [a, *b]
+
+        self.assertEqual(c, d)
+
+    def test_flatten(self):
+        a = [1, 2]
+        b = [3, 4]
+        c = [a, b]
+        d = [1, 2, 3, 4]
+
+        actual = [aa for aa in a]
