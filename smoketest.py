395e259652747303a1ce18760ecf414b33a97c0f
Nate Grigg <nate.grigg@endurance.com>
2021-07-22 14:12:39 -0600

    remove work file

--- smoketest.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'https://beta.recommendations.unifiedlayer.com/users/121734/recommendations'
-
-headers = {'referer': 'https://bluehost.com'}
-
-response = requests.get(url, headers=headers)
-
-print(f'status code: {response.status_code}')
