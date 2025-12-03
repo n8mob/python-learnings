cb4edece8346eb86335aaac992cc9d59218e4496
Nate Grigg <nate.grigg@modelop.com>
2025-12-03 16:03:11 -0700

    test unpacking None and appending from a None source list

--- test_unpacking.py
+++ test_unpacking.py
@@ -12,8 +12,8 @@ class TestUnpacking(TestCase):
 
     def test_unpack_dictionary(self):
         d = {
-            'test_key1': 'test_dict_value1',
-            'test_key2': 'test_dict_value2',
+          'test_key1': 'test_dict_value1',
+          'test_key2': 'test_dict_value2',
         }
 
         actual1, actual2 = d
@@ -32,3 +32,32 @@ class TestUnpacking(TestCase):
 
         self.assertIn('not enough', actual)
         self.assertIn('expected 4, got 2', actual)
+
+    # noinspection PyTupleAssignmentBalance
+    def test_unpack_none_list(self):
+        lst = None
+
+        with self.assertRaises(TypeError) as tex_context:
+            a, b = lst
+            self.assertIsNone(a)
+            self.assertIsNone(b)
+
+        actual = str(tex_context.exception)
+
+        self.assertIn('cannot unpack', actual)
+        self.assertIn('NoneType', actual)
+
+    # noinspection PyArgumentList
+    def test_append_none(self):
+        source_list = None
+        destination_list = ['not empty']
+
+        with self.assertRaises(TypeError) as tex_context:
+            destination_list.append(*source_list)
+
+        actual = str(tex_context.exception)
+
+        self.assertIn('iterable', actual)
+        self.assertIn('NoneType', actual)
+
+        self.assertEqual(['not empty'], destination_list, 'destination list should be unchanged')
